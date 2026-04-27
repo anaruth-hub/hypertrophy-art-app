a806de0d0c1efec62bdd0598d124eb4349d8ada1
Ana Ruth <aniarinna@gmail.com>
2026-04-27 10:20:26 +0200

    chore: initial project setup

---
+++ src/main/java/com/anaruth/hypertrophyartapp/HypertrophyArtAppApplication.java
@@ -0,0 +1,13 @@
+package com.anaruth.hypertrophyartapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypertrophyArtAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HypertrophyArtAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anaruth/hypertrophyartapp/HypertrophyArtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anaruth.hypertrophyartapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypertrophyArtAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
