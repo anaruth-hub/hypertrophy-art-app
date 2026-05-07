58aca2a9c70c0b808dacad3f45eb8552e4ca9eb3
Ana Ruth <aniarinna@gmail.com>
2026-05-07 20:48:10 +0200

    feat(persistence): persist users with PostgreSQL adapter

---
+++ src/main/java/com/anaruth/hypertrophyartapp/infrastructure/persistence/postgres/user/PostgresUserPersistenceAdapter.java
@@ -0,0 +1,34 @@
+package com.anaruth.hypertrophyartapp.infrastructure.persistence.postgres.user;
+
+import com.anaruth.hypertrophyartapp.application.user.port.out.UserRepository;
+import com.anaruth.hypertrophyartapp.domain.user.model.User;
+import com.anaruth.hypertrophyartapp.domain.user.model.UserId;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@Primary
+public class PostgresUserPersistenceAdapter implements UserRepository {
+
+    private final SpringDataUserJpaRepository springDataUserJpaRepository;
+    private final UserJpaMapper userJpaMapper = new UserJpaMapper();
+
+    public PostgresUserPersistenceAdapter(SpringDataUserJpaRepository springDataUserJpaRepository) {
+        this.springDataUserJpaRepository = springDataUserJpaRepository;
+    }
+
+    @Override
+    public User save(User user) {
+        UserEntity entity = userJpaMapper.toEntity(user);
+        UserEntity savedEntity = springDataUserJpaRepository.save(entity);
+        return userJpaMapper.toDomain(savedEntity);
+    }
+
+    @Override
+    public Optional<User> findById(UserId userId) {
+        return springDataUserJpaRepository.findById(userId.value().toString())
+                .map(userJpaMapper::toDomain);
+    }
+}
