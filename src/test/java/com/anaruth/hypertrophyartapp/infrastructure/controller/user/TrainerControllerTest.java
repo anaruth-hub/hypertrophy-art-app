ff1406862d00921bf843461697da7f17d617e69d
Ana Ruth <aniarinna@gmail.com>
2026-05-27 08:41:07 +0200

    test(trainer): add trainer controller test

---
+++ src/test/java/com/anaruth/hypertrophyartapp/infrastructure/controller/user/TrainerControllerTest.java
@@ -0,0 +1,52 @@
+package com.anaruth.hypertrophyartapp.infrastructure.controller.trainer;
+
+import com.anaruth.hypertrophyartapp.application.trainer.port.in.CreateTrainerResult;
+import com.anaruth.hypertrophyartapp.application.trainer.port.in.CreateTrainerUseCase;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(TrainerController.class)
+class TrainerControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private CreateTrainerUseCase createTrainerUseCase;
+
+    @Test
+    void shouldCreateTrainer() throws Exception {
+        UUID trainerId = UUID.randomUUID();
+
+        when(createTrainerUseCase.createTrainer(any()))
+                .thenReturn(new CreateTrainerResult(
+                        trainerId,
+                        "Coach Laura",
+                        "coach@test.com"
+                ));
+
+        mockMvc.perform(post("/api/trainers")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                {
+                                  "name": "Coach Laura",
+                                  "email": "coach@test.com"
+                                }
+                                """))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(trainerId.toString()))
+                .andExpect(jsonPath("$.name").value("Coach Laura"))
+                .andExpect(jsonPath("$.email").value("coach@test.com"));
+    }
+}
