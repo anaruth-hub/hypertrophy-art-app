b8ee57f6cefd4d2062b935b84f74f63a68bab1fa
Ana Ruth <aniarinna@gmail.com>
2026-06-04 07:00:38 +0200

    docs(auth): configure Swagger JWT authorization

--- src/main/java/com/anaruth/hypertrophyartapp/HypertrophyArtAppApplication.java
+++ src/main/java/com/anaruth/hypertrophyartapp/HypertrophyArtAppApplication.java
@@ -1,9 +1,11 @@
 package com.anaruth.hypertrophyartapp;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SecurityRequirement(name = "bearerAuth")
 public class HypertrophyArtAppApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/anaruth/hypertrophyartapp/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.anaruth.hypertrophyartapp.infrastructure.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Hypertrophy Art API",
+                version = "1.0",
+                description = "API for natural hypertrophy tracking"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
